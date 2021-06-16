11143bf59f651bacc35730ef650c7d17c297bac4
Chaitra <Chaitra@hp-pc>
2021-06-16 12:24:56 +0530

    User 1 says this is my first commit

---
+++ com.crm.USER1/src/main/java/com/crm/com/crm/USER1/App.java
@@ -0,0 +1,13 @@
+package com.crm.com.crm.USER1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ com.crm.USER1/src/test/java/com/crm/com/crm/USER1/User.java
@@ -0,0 +1,10 @@
+package com.crm.com.crm.USER1;
+
+public class User {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hi Im user 1");
+	}
+
+}
